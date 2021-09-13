d78b2464512ef0e79c7dd190a5e6bdac6f5b2145
YeliseyevaO <eliseevaolgaf5@gmail.com>
2021-09-13 11:45:51 +0300

    сделала ref

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 /*import './sass/main.scss';*/
-/*23352968-b8b048e55839ee6b2f6a0c2b8*/
+/*
+const options = {
+  headers: {
+    Authorization: '23352968-b8b048e55839ee6b2f6a0c2b8',
+  },
+};*/
+const refs = {
+  input: document.querySelector('#search-form'),
+  imgList: document.querySelector('.gallery'),
+};
+refs.input.addEventListener('input', debounce(foundFoto, 500));
 
+function foundFoto(e) {
+  fetch(
+    'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=sea&page=1&per_page=12&key=23352968-b8b048e55839ee6b2f6a0c2b8',
+  )
+    .then(r => r.json())
+    .then(console.log);
+}
