05568e9a5e55ed0fe55d74a16d4f0ca472faa586
YeliseyevaO <eliseevaolgaf5@gmail.com>
2021-09-16 15:54:17 +0300

    scroll

--- src/index.js
+++ src/index.js
@@ -2,33 +2,29 @@ import './sass/main.scss';
 import debounce from 'lodash.debounce';
 import PhotoApiService from './api-service';
 import photoCardTml from './photo.hbs';
-/*
-const options = {
-  headers: {
-    Authorization: '23352968-b8b048e55839ee6b2f6a0c2b8',
-  },
-};*/
+
 const photoApiServise = new PhotoApiService();
 const refs = {
   form: document.querySelector('#search-form'),
   imgList: document.querySelector('.gallery'),
-  moreBtn: document.querySelector('.more-btn'),
+  moreBtn: document.querySelector('.load-more'),
 };
 refs.form.addEventListener('input', debounce(foundPhoto, 500));
 refs.moreBtn.addEventListener('click', foundMorePhoto);
 
 function foundPhoto(e) {
+  refs.imgList.innerHTML = '';
   photoApiServise.photo = e.target.value;
   photoApiServise.stepOnOnePage();
   photoApiServise.fetchPhoto();
   photoApiServise.fetchPhoto().then(photoCardMarkUp);
 }
 function foundMorePhoto(e) {
-  photoApiServise.fetchPhoto().then(photoCardMarkUp);
   refs.imgList.scrollIntoView({
     behavior: 'smooth',
     block: 'end',
   });
+  photoApiServise.fetchPhoto().then(photoCardMarkUp);
 }
 
 function photoCardMarkUp(hits) {
